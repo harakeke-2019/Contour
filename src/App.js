4433c82d21cc89f5be101606ef654b15858d5b22
Aubrey Te Kanawa <aubreytekanawa@gmail.com>
2019-02-08 12:53:12 +1300

    added router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
 import React from 'react'
-import request from 'superagent'
+
+import Search from './Search'
+import {Route} from 'react-router-dom'
+
+
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      searchTerm: 'batman',
-      data: []
+     
     }
-    }
-  
-  componentDidMount () {
-    const URL = 'https://api.themoviedb.org'
-    const API_KEY = '81009ccb87e4c64254d9e74c695d113a'
-    const MOVIE_URL = `${URL}/3/search/movie?api_key=${API_KEY}&query=${this.state.searchTerm}`
-    request.get(MOVIE_URL)
-    // .then(console.log)
-    .then(res => {
-      this.setState ({
-        data: res.body.results
-      })
-    })
   }
 
-  
+render() { 
+return (
+  <div>
+    <Route path='/' component={Search}/>
+  </div>
+ )  
 
-render() {
-  console.log(this.state.data)
-  
-  const items  = this.state.data.map(el => {
-    return ( <li>{el.title}</li>)
-  }) 
-return  <React.Fragment>
-    {items}
-  </React.Fragment>
-
-  }
+ }
 }
+
 
 export default App
