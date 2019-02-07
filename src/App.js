daada536e011e7da1a22e575726b1c32e253df39
Aubrey Te Kanawa <aubreytekanawa@gmail.com>
2019-02-08 11:57:08 +1300

    first query working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React from 'react'
+import request from 'superagent'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      searchTerm: 'batman',
+      data: []
+    }
+    }
+  
+  componentDidMount () {
+    const URL = 'https://api.themoviedb.org'
+    const API_KEY = '81009ccb87e4c64254d9e74c695d113a'
+    const MOVIE_URL = `${URL}/3/search/movie?api_key=${API_KEY}&query=${this.state.searchTerm}`
+    request.get(MOVIE_URL)
+    // .then(console.log)
+    .then(res => {
+      this.setState ({
+        data: res.body.results
+      })
+    })
   }
+
+  
+
 render() {
-  return (
-  <div>
-  <h1>Hello world</h1>
-  </div>)
-}
+  console.log(this.state.data)
+  
+  const items  = this.state.data.map(el => {
+    return ( <li>{el.title}</li>)
+  }) 
+return  <React.Fragment>
+    {items}
+  </React.Fragment>
+
+  }
 }
 
 export default App
